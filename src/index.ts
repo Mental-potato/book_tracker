576def0e34dbb7c25ad46705679dc7d125d1a598
Mental-potato <153oorrii153@gmail.com>
2025-04-09 03:25:11 +0300

    f

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,3 @@ async function init() {
 }
 
 init();
-
-export async function initApp(root: HTMLPreElement) {
-    const res = await fetch('/api/hello');
-    const data = await res.json();
-
-    root.textContent = JSON.stringify(data, null, 2);
-}
